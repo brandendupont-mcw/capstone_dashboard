6af4ad33ac5278683332aeee1cfa26773a627ff9
brandendupont-mcw <bdupont@mcw.edu>
2022-06-06 23:03:18 -0400

    update

--- next.config.js
+++ next.config.js
@@ -54,6 +54,7 @@ const securityHeaders = [
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
+  basePath: '/pfa',
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   eslint: {
     dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
