d40bf35ea64c73d2737dfba33007dc40cf9a9bde
brandendupont-mcw <bdupont@mcw.edu>
2022-06-20 01:20:40 -0400

    add two data stories

--- components/comments/Background.js
+++ components/comments/Background.js
@@ -1,7 +1,7 @@
-export default function Background({ children }) {
+export default function Background({ children, title }) {
     return <div className="mb-4">
         <div className="bg-gray-100 px-4 pt-1 pb-1">
-            <h4> What does this mean for those arrested? </h4>
+            <h4> {title} </h4>
             {children}
         </div>
     </div>
